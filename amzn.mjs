5e5a189d94264e13090a434e2939cd5ff5f4e172
aryanbakshi25 <aryanbakshi2509@gmail.com>
2023-07-30 03:09:10 -0400

    out of stock function now works

--- amzn.mjs
+++ amzn.mjs
@@ -6,9 +6,11 @@ import { Webhook, MessageBuilder } from 'discord-webhook-node';
 
 
 
-//https://www.amazon.com/Bedroom-Bluetooth-Changing-Christmas-Decoration/dp/B09BJHWQHZ?pd_rd_w=YgSjl&content-id=amzn1.sym.deffa092-2e99-4e9f-b814-0d71c40b24af&pf_rd_p=deffa092-2e99-4e9f-b814-0d71c40b24af&pf_rd_r=3F137CP5H9Y3MGX39Z0Z&pd_rd_wg=9KD9p&pd_rd_r=90bd687d-5f7b-46ff-ac25-443c3f561208&pd_rd_i=B09BJHWQHZ&ref_=pd_bap_d_grid_rp_0_1_t&th=1, https://www.amazon.com/Damn-Exclusive-Limited-Translucent-Colored/dp/B08JX1D2ZF/ref=sr_1_11?crid=1YTR1GIHFQXP2&keywords=kendrick+lamar+vinyl&qid=1674291848&refinements=p_n_availability%3A2661601011&rnid=2661599011&sprefix=kendrick+%2Caps%2C136&sr=8-11
+//https://www.amazon.com/Bedroom-Bluetooth-Changing-Christmas-Decoration/dp/B09BJHWQHZ?pd_rd_w=YgSjl&content-id=amzn1.sym.deffa092-2e99-4e9f-b814-0d71c40b24af&pf_rd_p=deffa092-2e99-4e9f-b814-0d71c40b24af&pf_rd_r=3F137CP5H9Y3MGX39Z0Z&pd_rd_wg=9KD9p&pd_rd_r=90bd687d-5f7b-46ff-ac25-443c3f561208&pd_rd_i=B09BJHWQHZ&ref_=pd_bap_d_grid_rp_0_1_t&th=1
+//https://www.amazon.com/Damn-Exclusive-Limited-Translucent-Colored/dp/B08JX1D2ZF/ref=sr_1_11?crid=1YTR1GIHFQXP2&keywords=kendrick+lamar+vinyl&qid=1674291848&refinements=p_n_availability%3A2661601011&rnid=2661599011&sprefix=kendrick+%2Caps%2C136&sr=8-11
+// Sample Out of Stock Item -> https://www.amazon.com/Masters-Universe-Masterverse-Collectible-Articulations/dp/B0BCN47F77/ref=sr_1_62?crid=16D2A7NNNT87Q&keywords=Masters+of+the+Universe+Castle+Grayskull+Giftset+with+bonus+figures&qid=1690699957&refinements=p_n_availability%3A2661601011&rnid=2661599011&sprefix=masters+of+the+universe+castle+grayskull+giftset+with+bonus+figures%2Caps%2C87&sr=8-62
 
-const hook = new Webhook("https://discord.com/api/webhooks/1065395648192585908/a8cjti9DeAY66bmNY__LoXtnTusK1vp3o576cR6Ai-zOmEKLCbnnx0S3SVCplaGTWRda");
+const hook = new Webhook("https://discord.com/api/webhooks/1135098009504469052/Q2GNfNG6jN4PiTD69xo041KkwGNvOYhFxWFxAxu8FOoGooX0EGEzAf6ESW9zBJRMGcWy");
  
 const embed = new MessageBuilder()
 .setTitle('Amazon Stock Status Monitor')
@@ -45,7 +47,12 @@ async function Monitor(productLink){
             let productImageURL = root.querySelector('#landingImage').getAttribute('src');
             let productName = productLink.substring(productLink.indexOf('com/') + 4, productLink.indexOf('/dp'));
             let stockText = availabilityDiv.childNodes[1].innerText.toLowerCase();
-            if(stockText == 'out of stock'){
+            if(stockText.includes('out of stock')){
+                embed.setColor('#FF0000')
+                embed.setThumbnail(productImageURL);
+                embed.addField(productName, productLink, true);
+                embed.addField('Availability', 'OUT OF STOCK', false);
+                hook.send(embed);
                 console.log(productName + ' OUT OF STOCK');
             }else{
                 embed.setThumbnail(productImageURL);
@@ -67,7 +74,7 @@ async function Run(){
     
     var productLinksArr = productLinks.split(',');
 
-    for(var i = 0; i < productLinksArr.length; i++){
+    for (var i = 0; i < productLinksArr.length; i++){
         productLinksArr[i] = productLinksArr[i].trim();
     }
     
@@ -81,7 +88,7 @@ async function Run(){
         monitors.push(p);
     });
     
-    console.log('Now monitoring ' + productLinksArr.length + ' items');
+    console.log('Now monitoring ' + productLinksArr.length + ' item(s)');
     await Promise.all(monitors);
 
 }
